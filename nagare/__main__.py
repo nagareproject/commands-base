f616ca7c7863056037d526f4091a70c7cf691216
Alain Poirier <alain.poirier@net-ng.com>
2019-02-25 19:20:34 +0100

    `python -m nagare` and `python -m nagare.admin`

---
+++ nagare/__main__.py
@@ -0,0 +1,14 @@
+# --
+# Copyright (c) 2008-2019 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+import sys
+
+from nagare.admin import admin
+
+sys.exit(admin.run())
